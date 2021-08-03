4a980a59beaaafb0c1dcda56f26c6d44c94f14fb
Alptahta <alp.tahta@protonmail.com>
2021-08-03 15:09:49 +0300

    add comment for difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,15 +1,19 @@
+// Package diffsquares implements functions to calculate sum of squares
+// squares of sum and difference between them
 package diffsquares
 
+// SquareOfSum calculates squares of sum of numbers to n
 func SquareOfSum(n int) int {
 	squareSum := (n * (n + 1)) / 2
 	return squareSum * squareSum
 }
 
+// SumOfSquares calculates sum of squares of numbers to n
 func SumOfSquares(n int) int {
-
 	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
+// Difference calculates difference between two functions at the above
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
